0492caa883ea20351f8543850c28735f42adaf5c
Julienpetit951 <julienpetit951@gmail.com>
2024-11-14 21:35:42 +0100

    create event model
    Création du modèle event

--- database/migrations/2024_11_07_190043_add_columns_to_users_table.php
+++ database/migrations/2024_11_07_190043_add_columns_to_users_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('firstname');
+            $table->string('firstname')->nullable();
             $table->string('phone')->nullable();
-            $table->string('login');
+            $table->string('login')->nullable();
         });
     }
 
