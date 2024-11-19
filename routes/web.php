e49ed385d45bf46190e7c819a4b204f3d5114d9a
Julienpetit951 <julienpetit951@gmail.com>
2024-11-19 19:54:00 +0100

    update routes
    Mise à jour des routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -22,7 +24,34 @@ Route::get('/greeting', function () {
     return 'Hello World';
 });
 
-//user.index
+//User
 Route::controller(UserController::class)->name('user.')->group(function () {
     Route::get('/users', 'show')->name('index');
+    Route::get('/users', 'show')->name('create');
+    Route::get('/users', 'show')->name('destroy');
+    Route::get('/users', 'show')->name('update');
+    Route::get('/users', 'show')->name('edit');
+    Route::get('/users', 'show')->name('show');
+    Route::get('/users', 'show')->name('store');
+});
+
+//Reservation
+Route::controller(ReservationController::class)->name('reservation.')->group(function () {
+    Route::get('/reservation', 'show')->name('index');
+    Route::get('/reservation', 'show')->name('create');
+    Route::get('/reservation', 'show')->name('destroy');
+    Route::get('/reservation', 'show')->name('update');
+    Route::get('/reservation', 'show')->name('edit');
+    Route::get('/reservation', 'show')->name('show');
+    Route::get('/reservation', 'show')->name('store');
+});
+
+Route::controller(EventController::class)->name('event.')->group(function () {
+    Route::get('/event', 'show')->name('index');
+    Route::get('/event', 'show')->name('create');
+    Route::get('/event', 'show')->name('destroy');
+    Route::get('/event', 'show')->name('update');
+    Route::get('/event', 'show')->name('edit');
+    Route::get('/event', 'show')->name('show');
+    Route::get('/event', 'show')->name('store');
 });
